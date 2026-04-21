e74df06b0abc33f8584d591f46d18fd24f297c5a
Guilherme Franco <francoguilherme.sec@gmail.com>
2026-04-21 12:54:43 -0300

    :sparkles:  feat =>  adicionado configuração de cors para o dominio da aplicação

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://app.truechurch.com.br',
+      'http://localhost:3000',
+      'http://localhost:3005'
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
